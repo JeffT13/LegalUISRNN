1dabcdd92636d43149ca1ccebdd0649c09d33808
JeffT13 <jefftumminia@gmail.com>
2020-11-20 16:59:59 -0500

    checking scotus dvecs

--- SCOTUS.py
+++ SCOTUS.py
@@ -2,14 +2,26 @@
 
 import numpy as np
 import torch
+import glob
+import os
 import uisrnn
+
+
 
 '''
 Things to look into:
-	- loading training/testing data in
-	- args?
-	- multiple .fit() calls
-	- input concatenation
+	- arg:
+
+        MODEL
+        = estimatable:
+                - transition_bias (eq 13) (prob should estimate)
+                - sigma2 (eq 11)
+        = crp_alpha (eq 7, *cannot be estimated)
+        TRAINING
+        = batch_size?
+	= num_permutations?
+
+	- multiple .fit() calls & input concatenation
 	- labels to string
 '''
 
@@ -18,26 +30,42 @@ train_seqs = []
 train_labels = []
 test_items = []
 
-train_case_path = './data/SCOTUS_Processed/train/*'
-test_case_path = './data/SCOTUS_Processed/test/*'
+train_case_path = './data/SCOTUS_Processed/train/*/*'
+#test_case_path = './data/SCOTUS_Processed/test/*'
 
+case_path = glob.glob(os.path.dirname(train_case_path))
+
+print(case_path)
 #load training data
-for case in enumerate(train_case_path):
-	case_id = case.split('/')
+
+train_sequences = []
+train_cluster_ids = []
+for i, case in enumerate(case_path):
+	print(case)
+	case_id = case.split('/')[-1][:-7]
+	print(case_id)
 	temp_seq = np.load(case+'/'+case_id+'_emb.npy', allow_pickle=True)
 	temp_lab = np.load(case+'/'+case_id+'_label.npy', allow_pickle=True)
-	train_seq.append(temp_seq)
-	train_labels.append(temp_lab)
-	
+	if True:
+		print(np.shape(temp_seq))
+		print(np.shape(temp_lab))
+	train_sequences.append(temp_seq)
+	train_cluster_ids.append(temp_lab)
+	break
+
 
 #Define UISRNN
 model_args, training_args, inference_args = uisrnn.parse_arguments()
-model = uisrnn.UISRNN(args)
+model_args.verbosity=3
+model_args.observation_dim=256 #from hparam techincally
+
+training_args.enforce_cluster_id_uniqueness=False #based on dvec_SCOTUS
+training_args.batch_size = 3
 
 #model training **MUST BE LOOP TO MANAGE SEQ LENGTH**
-model.fit(train_sequences, train_cluster_ids, args, enforce_cluster_id_uniqueness=False)
-
+model = uisrnn.UISRNN(model_args)
+model.fit(train_sequences, train_cluster_ids, training_args)
 
 #evaluation has similar mechanic
-predicted_cluster_ids = model.predict(test_sequences, args)
+#predicted_cluster_ids = model.predict(test_sequences, inference_args)
 
---
+++ checkdata.py
@@ -0,0 +1,28 @@
+import numpy as np
+import uisrnn
+
+'''
+- check toy data shape
+- import case, generate Z
+'''
+
+toy_train = np.load('data/toy_training_data.npz')
+toy_test = np.load('data/toy_testing_data.npz', allow_pickle=True)
+
+print('Train:', toy_train.files)
+print(np.shape(toy_train['train_sequence']))
+print(np.shape(toy_train['train_cluster_id']))
+print(np.unique(toy_train['train_cluster_id']))
+print(np.shape(np.unique(toy_train['train_cluster_id'])))
+
+print(type(toy_train['train_cluster_id'][0]))
+print('Test:', toy_test.files)
+print(np.shape(toy_test['test_sequences']))
+print(np.shape(toy_test['test_cluster_ids']))
+print(np.shape(np.unique(toy_test['test_cluster_ids'])))
+print(np.shape(toy_test['test_sequences'][0]))
+print(np.unique(toy_test['test_cluster_ids'][0]))
+print(np.shape(np.unique(toy_test['test_cluster_ids'][0])))
+print(np.shape(toy_test['test_sequences'][1]))
+print(np.unique(toy_test['test_cluster_ids'][1]))
+print(np.shape(np.unique(toy_test['test_cluster_ids'][1])))
