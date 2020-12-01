238c2e62fc6da4d97f83ad36dc51648578d4f62b
JeffT13 <jefftumminia@gmail.com>
2020-12-01 18:43:33 -0500

    gpu in int

--- SCOTUS/script/intcheck.sh
+++ SCOTUS/script/intcheck.sh
@@ -3,8 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1:00:00
+#SBATCH --gres=gpu:1
 #SBATCH --mem=8GB
 #SBATCH --job-name=integrate
-#SBATCH --output=intchck_uisrnn.out
+#SBATCH --output=intchck.out
 
 python LegalUISRNN/SCOTUS/testing/analyzeintegration.py
